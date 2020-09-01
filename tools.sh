222e3f929ddad2f3a3efd5da0973fbcec54fb1c7
nvk <naveenkumawat1995@gmail.com>
2020-09-02 02:25:21 +0530

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -18,7 +18,7 @@ sudo apt-get install -y rename
 sudo apt-get install -y xargs
 
 echo "installing bash_profile aliases from recon_profile"
-git clone https://github.com/nahamsec/recon_profile.git
+git clone https://github.com/nvk0x/nvk_bbht.git
 cd recon_profile
 cat bash_profile >> ~/.bash_profile
 source ~/.bash_profile
